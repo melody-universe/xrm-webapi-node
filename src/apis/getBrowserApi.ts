f632f818c61407c2264764932a9e1bd26a0263b8
melody-universe <tyler.hogsett@microsoft.com>
2021-10-11 07:28:47 -0700

    attempt to retrieve the WebApi from window.parent if it's not on window

--- src/apis/getBrowserApi.ts
+++ src/apis/getBrowserApi.ts
@@ -3,6 +3,8 @@ import { Api } from "../types/Api.js";
 export default function getBrowserApi(): Api {
   if (hasXrmWebApi(window)) {
     return window.Xrm.WebApi;
+  } else if (hasXrmWebApi(window.parent)) {
+    return window.parent.Xrm.WebApi;
   } else {
     throw new Error("Unable to resolve Dataverse API");
   }
@@ -16,6 +18,5 @@ function hasXrm(window: Window): window is XrmWindow {
   return "Xrm" in window;
 }
 
-type Window = typeof window;
 type XrmWindow = Window & { Xrm: any };
 type XrmWebApiWindow = Window & { Xrm: { WebApi: Api } };
