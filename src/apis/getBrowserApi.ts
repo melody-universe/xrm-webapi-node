31759dae2837c183a50453107e2bbc8166139c12
melody-universe <tyler.hogsett@microsoft.com>
2021-10-11 10:38:48 -0700

    adjusted for stateful Xrm.WebApi objects

--- src/apis/getBrowserApi.ts
+++ src/apis/getBrowserApi.ts
@@ -2,9 +2,9 @@ import { Api } from "../types/Api.js";
 
 export default function getBrowserApi(): Api {
   if (hasXrmWebApi(window)) {
-    return window.Xrm.WebApi;
+    return wrapWindowApi(window);
   } else if (hasXrmWebApi(window.parent)) {
-    return window.parent.Xrm.WebApi;
+    return wrapWindowApi(window.parent);
   } else {
     throw new Error("Unable to resolve Dataverse API");
   }
@@ -20,3 +20,11 @@ function hasXrm(window: Window): window is XrmWindow {
 
 type XrmWindow = Window & { Xrm: any };
 type XrmWebApiWindow = Window & { Xrm: { WebApi: Api } };
+
+function wrapWindowApi(container: XrmWebApiWindow): Api {
+  return {
+    createRecord: container.Xrm.WebApi.createRecord.bind(container.Xrm.WebApi),
+    retrieveMultipleRecords: container.Xrm.WebApi.retrieveMultipleRecords.bind(container.Xrm.WebApi),
+    retrieveRecord: container.Xrm.WebApi.retrieveRecord.bind(container.Xrm.WebApi),
+  };
+}
