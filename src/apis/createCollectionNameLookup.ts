4204e09318557fee241dc9d323e2006f6bf15907
melody-universe <tyler.hogsett@microsoft.com>
2021-10-14 19:31:49 -0700

    removed dependency on odata-query

--- src/apis/createCollectionNameLookup.ts
+++ src/apis/createCollectionNameLookup.ts
@@ -1,6 +1,4 @@
 import fetch, { HeadersInit } from "node-fetch";
-import odataQuery from "odata-query";
-const buildQuery = (odataQuery as unknown as any).default as typeof odataQuery;
 import { EntityDefinition } from "../types/EntityDefinition.js";
 
 export function createCollectionNameLookup(
@@ -20,14 +18,14 @@ export function createCollectionNameLookup(
   };
 
   async function getCollectionName(entityName: string) {
-    const query = buildQuery<EntityDefinition>({
-      key: { LogicalName: entityName },
-      select: ["LogicalCollectionName"],
-    });
-    const response = await fetch(`${apiBaseUrl}/EntityDefinitions${query}`, {
-      headers: await headersPromise,
-      method: "GET",
-    });
+    const response = await fetch(
+      `${apiBaseUrl}/EntityDefinitions(LogicalName='${entityName}')?` +
+        `$select=LogicalCollectionName`,
+      {
+        headers: await headersPromise,
+        method: "GET",
+      }
+    );
     if (response.ok) {
       const definition = (await response.json()) as EntityDefinition;
       console.log(definition);
